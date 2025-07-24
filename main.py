96cd886ad9744a1186b338feec9e4404f3a50dd2
Shubham Singh <shubhamproject09882@gmail.com>
2025-07-24 22:12:31 +0530

    create DB and user model

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from extensions import db
 
 def create_app():
 
     app = Flask(__name__)
 
     app.config.from_prefixed_env()
+
+
+    db.init_app(app)
 
     return app
 
---
+++ models.py
@@ -0,0 +1,14 @@
+from extensions import db
+from uuid import uuid4
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.String(), primary_key=True, default=str(uuid4()))
+    usernam = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=False)
+    password = db.Column(db.Text())
+
+    def __repr__(self):
+        return f"<User {self.usernam}>"
+    
