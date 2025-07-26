59f6cd4f61faff4b8611ccdf756735fb66d8d4be
Shubham Singh <shubhamproject09882@gmail.com>
2025-07-26 21:48:01 +0530

    Login and create jwt tokens

--- auth.py
+++ auth.py
@@ -1,14 +1,15 @@
 from flask import Blueprint, jsonify, request
 from models import User
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 auth_bp = Blueprint('auth',__name__)
 
 @auth_bp.post('/register')
-def registration():
+def registration_user():
     data = request.get_json()
     user = User.get_user_by_username(username=data.get('username'))
     if user is not None:
-        return jsonify({'error':'USer already exists'}), 409
+        return jsonify({"error":"USer already exists"}), 409
     
     new_user = User(
         username= data.get('username'),
@@ -18,4 +19,20 @@ def registration():
     new_user.save() 
     return jsonify({"message" : "User Created"}), 201
     
+
+@auth_bp.post('/login')
+def login_user():
     
+    data = request.get_json()
+    user = User.get_user_by_username(username=data.get('username'))
+    if user and user.check_password(password=data.get('password')):
+        access_token = create_access_token(identity=user.username)
+        refresh_token = create_refresh_token(identity=user.username)
+
+        return jsonify({
+            "message" : "Logged In",
+            "access" : access_token,
+            "refresh" : refresh_token
+        }), 200
+    
+    return jsonify({"error" : "Invalid username or password"}), 400
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from extensions import db
+from extensions import db, jwt
 from auth import auth_bp
 
 def create_app():
@@ -9,6 +9,8 @@ def create_app():
     app.config.from_prefixed_env()
     
     db.init_app(app)
+    jwt.init_app(app)
+
 
     app.register_blueprint(auth_bp,url_prefix='/auth')
 
