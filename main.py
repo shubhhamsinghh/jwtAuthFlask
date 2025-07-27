7cc7efd9199ba0a6c639435afb0f250ec61c2295
Shubham Singh <shubhamproject09882@gmail.com>
2025-07-27 19:59:53 +0530

    add jwt token handlers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from extensions import db, jwt
 from auth import auth_bp
 from users import user_bp
@@ -15,6 +15,19 @@ def create_app():
 
     app.register_blueprint(auth_bp,url_prefix='/auth')
     app.register_blueprint(user_bp,url_prefix='/users')
+
+    # jwt error handlers
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_data):
+        return jsonify({"message" : "Token has expired", "error" : "token_expired"}), 401
+    
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({"message" : "Signature varification failed", "error" : "invalid_token"}), 401
+    
+    @jwt.unauthorized_loader
+    def unauthorized_loader_callback(error):
+        return jsonify({"message" : "Request doesn't contain valid token", "error" : "authorization_header"}), 401
 
     return app
 
